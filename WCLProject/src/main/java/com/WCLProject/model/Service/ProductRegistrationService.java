434e7823f65544660f438b3f0f16f2e5cf00f7e9
DevHaeng <USER@DESKTOP-8MIDR267G>
2024-07-25 17:32:30 +0900

    - 미사용 파일 삭제

--- WCLProject/src/main/java/com/WCLProject/model/Service/ProductRegistrationService.java
+++
@@ -1,7 +0,0 @@
-package com.WCLProject.model.Service;
-
-import com.WCLProject.model.DTO.ProductDTO;
-
-public interface ProductRegistrationService {
-//	void registerProduct(ProductDTO product) throws ServiceException;
-}
