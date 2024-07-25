d11882c10f0055d943836b03ca1190fb63d9d6e0
USER <USER@DESKTOP-8MIDR267G>
2024-07-25 14:27:26 +0900

    - 메소드 주석 추가

--- WCLProject/src/main/java/com/WCLProject/model/DAO/DBUtil.java
+++ WCLProject/src/main/java/com/WCLProject/model/DAO/DBUtil.java
@@ -10,12 +10,14 @@ public class DBUtil {
 	private static final String URL = "jdbc:oracle:thin:@project-db-stu3.smhrd.com:1524:xe";
 	private static final String USER = "Insa5_SpringA_hacksim_3";
 	private static final String PASSWORD = "aishcool3";
-
+	
+	// DB연결
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		return DriverManager.getConnection(URL, USER, PASSWORD);
 	}
 
+	// DB연결 종료
 	public static void closeConnection(ResultSet rs, PreparedStatement pst, Connection conn) {
 		try {
 			if (rs != null) {
