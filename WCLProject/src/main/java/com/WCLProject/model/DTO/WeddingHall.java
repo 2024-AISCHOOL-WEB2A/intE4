609d02f885ef9f0d963ae8a8d995312eb1465664
DESKTOP-8MI2351\USER <gkhgudwp2020@gmail.com>
2024-07-27 02:07:59 +0900

    -오타 수정(나영) getweddingHallTitle()->getWeddingHallTitle()

--- WCLProject/src/main/java/com/WCLProject/model/DTO/WeddingHall.java
+++ WCLProject/src/main/java/com/WCLProject/model/DTO/WeddingHall.java
@@ -14,93 +14,73 @@ public class WeddingHall {
     private String vendorId;
     private String photoPath;
     private String weddingHallTitle;
-
-    // Getters and setters
-    public String getWeddingHallId() {
-        return weddingHallId;
-    }
-
-    public void setWeddingHallId(String weddingHallId) {
-        this.weddingHallId = weddingHallId;
-    }
-
-    public String getWeddingHallBrand() {
-        return weddingHallBrand;
-    }
-
-    public void setWeddingHallBrand(String weddingHallBrand) {
-        this.weddingHallBrand = weddingHallBrand;
-    }
-
-    public int getWeddingHallMealCost() {
-        return weddingHallMealCost;
-    }
-
-    public void setWeddingHallMealCost(int weddingHallMealCost) {
-        this.weddingHallMealCost = weddingHallMealCost;
-    }
-
-    public String getWeddingHallAssurance() {
-        return weddingHallAssurance;
-    }
-
-    public void setWeddingHallAssurance(String weddingHallAssurance) {
-        this.weddingHallAssurance = weddingHallAssurance;
-    }
-
-    public String getWeddingHallType() {
-        return weddingHallType;
-    }
-
-    public void setWeddingHallType(String weddingHallType) {
-        this.weddingHallType = weddingHallType;
-    }
-
-    public String getWeddingHallPrice() {
-        return weddingHallPrice;
-    }
-
-    public void setWeddingHallPrice(String weddingHallPrice) {
-        this.weddingHallPrice = weddingHallPrice;
-    }
-
-    public String getWeddingHallContent() {
-        return weddingHallContent;
-    }
-
-    public void setWeddingHallContent(String weddingHallContent) {
-        this.weddingHallContent = weddingHallContent;
-    }
-
-    public Timestamp getWeddingHallDate() {
-        return weddingHallDate;
-    }
-
-    public void setWeddingHallDate(Timestamp weddingHallDate) {
-        this.weddingHallDate = weddingHallDate;
-    }
-
-    public String getVendorId() {
-        return vendorId;
-    }
-
-    public void setVendorId(String vendorId) {
-        this.vendorId = vendorId;
-    }
-
-    public String getPhotoPath() {
-        return photoPath;
-    }
-
-    public void setPhotoPath(String photoPath) {
-        this.photoPath = photoPath;
-    }
     
-    public String getweddingHallTitle() {
-        return weddingHallTitle;
-    }
+	public String getWeddingHallId() {
+		return weddingHallId;
+	}
+	public void setWeddingHallId(String weddingHallId) {
+		this.weddingHallId = weddingHallId;
+	}
+	public String getWeddingHallBrand() {
+		return weddingHallBrand;
+	}
+	public void setWeddingHallBrand(String weddingHallBrand) {
+		this.weddingHallBrand = weddingHallBrand;
+	}
+	public int getWeddingHallMealCost() {
+		return weddingHallMealCost;
+	}
+	public void setWeddingHallMealCost(int weddingHallMealCost) {
+		this.weddingHallMealCost = weddingHallMealCost;
+	}
+	public String getWeddingHallAssurance() {
+		return weddingHallAssurance;
+	}
+	public void setWeddingHallAssurance(String weddingHallAssurance) {
+		this.weddingHallAssurance = weddingHallAssurance;
+	}
+	public String getWeddingHallType() {
+		return weddingHallType;
+	}
+	public void setWeddingHallType(String weddingHallType) {
+		this.weddingHallType = weddingHallType;
+	}
+	public String getWeddingHallPrice() {
+		return weddingHallPrice;
+	}
+	public void setWeddingHallPrice(String weddingHallPrice) {
+		this.weddingHallPrice = weddingHallPrice;
+	}
+	public String getWeddingHallContent() {
+		return weddingHallContent;
+	}
+	public void setWeddingHallContent(String weddingHallContent) {
+		this.weddingHallContent = weddingHallContent;
+	}
+	public Timestamp getWeddingHallDate() {
+		return weddingHallDate;
+	}
+	public void setWeddingHallDate(Timestamp weddingHallDate) {
+		this.weddingHallDate = weddingHallDate;
+	}
+	public String getVendorId() {
+		return vendorId;
+	}
+	public void setVendorId(String vendorId) {
+		this.vendorId = vendorId;
+	}
+	public String getPhotoPath() {
+		return photoPath;
+	}
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+	public String getWeddingHallTitle() {
+		return weddingHallTitle;
+	}
+	public void setWeddingHallTitle(String weddingHallTitle) {
+		this.weddingHallTitle = weddingHallTitle;
+	}
 
-    public void setWeddingHallTitle(String weddingHallTitle) {
-        this.weddingHallTitle = weddingHallTitle;
-    }
+    
 }
